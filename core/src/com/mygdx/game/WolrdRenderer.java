afa6c260083e646440e0b2add09c438fc59ea052
nongharunana <anongnat.rat@ku.th>
2016-11-17 15:15:27 +0700

    add score

--- core/src/com/mygdx/game/WolrdRenderer.java
+++ core/src/com/mygdx/game/WolrdRenderer.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
@@ -15,6 +16,7 @@ public class WolrdRenderer {
 	private Texture boyImg;
 	private Texture burnBoyImg;
 	private Texture bombImg;
+	private BitmapFont font;
 	public WolrdRenderer(EvilPanGame evilPanGame, World world) {
         this.evilPanGame = evilPanGame;
         batch = evilPanGame.batch;
@@ -24,12 +26,14 @@ public class WolrdRenderer {
         burnBoyImg = new Texture("boy3");
         chopsticksImg = new Texture("Chopsticks");
         bombImg = new Texture("Bomb");
+        font = new BitmapFont();
     }
 	public void render(float delta) {
 		Vector2 position = world.getChopsticks().getPosition();
         batch.begin(); 
         batch.draw(bgImg, 0, 0);
         batch.draw(chopsticksImg, position.x,position.y);
+        font.draw(batch, "score : " + world.getScore(), 200, 60);
         batch.end();
 
 	}
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -3,14 +3,19 @@ package com.mygdx.game;
 public class World {
 	private Chopsticks chopsticks;
     private EvilPanGame evilPanGame;
- 
+    private int score;
     World(EvilPanGame evilPanGame) {
         this.evilPanGame = evilPanGame;
- 
-       chopsticks = new Chopsticks(100,100,this);
+        score = 0;
+        chopsticks = new Chopsticks(100,100,this);
+    }
+    public int getScore() {
+        return score;
     }
     
-    
+    public void increaseScore() {
+        score += 5;
+    }
  
     Chopsticks getChopsticks() {
         return chopsticks;
