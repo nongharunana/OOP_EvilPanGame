fdf17fcb5dedd3a3464b8e351b6c0136220661a7
nongharunana <anongnat.rat@ku.th>
2016-11-17 16:45:28 +0700

    add gameover and pause

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -14,20 +14,37 @@ public class GameScreen extends ScreenAdapter{
 	 World world;
 	 WolrdRenderer worldRenderer;
 	 private Texture bgImg;
+	private Texture pauseImg;
+	 
+	 public static int status_screen;
+	 public static final int SCREEN_GAME = 0;
+	 public static final int SCREEN_PAUSE = 2;
+	 public static final int SCREEN_GAMEOVER = -1; 
+	 public static final int SCREEN_GAMESTART = 1;
 	 public GameScreen(EvilPanGame evilPanGame) {
 	        this.evilPanGame = evilPanGame;
 	        chopsticksImg = new Texture("Chopsticks");
 	        bgImg = new Texture("bg");
 	        world = new World(evilPanGame);
+	        pauseImg = new Texture("Pause.png");
 	        worldRenderer = new WolrdRenderer(evilPanGame, world);
 	      
 	    }
 	 @Override
 	    public void render(float delta) {
+		 	SpriteBatch batch = evilPanGame.batch;
 		 	update(delta);
+		 	updateGameScreen();
+		 	if(status_screen == SCREEN_GAME){
 	        Gdx.gl.glClearColor(0, 0, 0, 1);
 	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	        worldRenderer.render(delta);
+	        }
+		 	else if(status_screen == SCREEN_PAUSE){
+		 		batch.begin();
+		 		batch.draw(pauseImg, 0, 0);
+		 		batch.end();
+		 	}
 	    }
 	  private void update(float delta) {
 		    if(Gdx.input.isKeyPressed(Keys.LEFT)) {
@@ -37,4 +54,18 @@ public class GameScreen extends ScreenAdapter{
 	        	world.getChopsticks().move(world.getChopsticks().DIRECTION_RIGHT);
 	        }   
 	    }
+	  private void updateGameScreen(){
+		  if(Gdx.input.isKeyPressed(Keys.ENTER)) {
+			  status_screen = SCREEN_GAMESTART;
+		  } else if(status_screen == SCREEN_GAME){
+			  if(Gdx.input.isKeyPressed(Keys.P)){
+			  status_screen = SCREEN_PAUSE;
+			  }
+		  }else if(status_screen == SCREEN_PAUSE){
+				  if(Gdx.input.isKeyPressed(Keys.SPACE)){
+					  status_screen = SCREEN_GAME;
+					  }
+		  }
+	  }
+	  
 }
